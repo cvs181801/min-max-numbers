1cdaa24481e9fbb01726fe0f3e603e475a2ce567
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 10:55:09 -0700

    remove the letter from the array, try again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //create a way to find the maximum and minimum numbers in any number set.
 
-const numArray = [2, 5, 17, 0, 9, -3, 'P'];
+const numArray = [2, 5, 17, 0, 9, -3];
 
 
 
@@ -10,11 +10,11 @@ function findMinMax(numArray) {
     const findMax = Math.max(...numArray);
     const findMin = Math.min(...numArray);
     console.log(findMax, findMin);
-    // if (findMax === NaN) {
-    //     return `Oops, try it using only numbers instead!`;
-    // } else {
-    //     return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
-    // }
+    if (findMax === NaN) {
+        return `Oops, try it using only numbers instead!`;
+    } else {
+        return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
+    }
 }
 
 console.log(findMinMax(numArray));
