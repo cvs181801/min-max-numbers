99dfaa87c9877c425ff7ddcf3d87ff0e6f73181c
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 11:23:50 -0700

    test the .push() out

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 
+//grab the elements 
+
+//const randomNumberSet = document.getElementById("randomNumbers");
+const goButton = document.getElementById("go-btn");
+const parentDiv = getDocumentById("resultDiv");
+//const childDiv = document.createElement("p");
+
 
 //create a way to find the maximum and minimum numbers in any number set.
+randomNumberSet = [4, 8, 9, 2, -10];
+const numArray = [];
 
-const numArray = [2, 5, 17, 0, 9, -3, 'P'];
 
 
-
-function findMinMax(numArray) {
+function findMinMax(randomNumberSet) {
+    numArray.push(randomNumberSet);
     const findMax = Math.max(...numArray);
     const findMin = Math.min(...numArray);
     console.log(findMax, findMin);
     if (Number.isNaN(findMax)) {
         return `Oops, try it using only numbers instead!`;
     } else {
+        //const childDiv = document.createElement("p");
         return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
     }
 }
 
-console.log(findMinMax(numArray));
+console.log(findMinMax(randomNumberSet));
+
