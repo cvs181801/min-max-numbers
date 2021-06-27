b35f315c87a4ec6fecbedb32bbd2da5b5a4f2e3d
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 10:48:37 -0700

    add an if else statement

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const numArray = [2, 5, 17, 0, 9, -3, 'P'];
 
 
 function findMinMax(numArray) {
+    
     const findMax = Math.max(...numArray);
     const findMin = Math.min(...numArray);
-   return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
+    if (NaN) {
+        return `Oops, try it using only numbers instead!`;
+    }
+    return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
 }
 
 console.log(findMinMax(numArray));
