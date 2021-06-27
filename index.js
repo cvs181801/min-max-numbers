8ca52bab46ec4e8d4a57846428ed9e46fb42c79a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 10:53:05 -0700

    try it with a console log

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const numArray = [2, 5, 17, 0, 9, -3, 'P'];
 function findMinMax(numArray) {
     const findMax = Math.max(...numArray);
     const findMin = Math.min(...numArray);
-    if (findMax === NaN) {
-        return `Oops, try it using only numbers instead!`;
-    } else {
-        return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
-    }
+    console.log(findMax, findMin);
+    // if (findMax === NaN) {
+    //     return `Oops, try it using only numbers instead!`;
+    // } else {
+    //     return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
+    // }
 }
 
 console.log(findMinMax(numArray));
