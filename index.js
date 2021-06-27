37d6d3ad9a03883580f0651a35ea164a9ea2f115
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 11:04:59 -0700

    try adding a strgin back in, test again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //create a way to find the maximum and minimum numbers in any number set.
 
-const numArray = [2, 5, 17, 0, 9, -3];
+const numArray = [2, 5, 17, 0, 9, -3, 'P'];
 
 
 
@@ -10,7 +10,7 @@ function findMinMax(numArray) {
     const findMax = Math.max(...numArray);
     const findMin = Math.min(...numArray);
     console.log(findMax, findMin);
-    if (Number.isNaN(NaN)) {
+    if (Number.isNaN(findMax)) {
         return `Oops, try it using only numbers instead!`;
     } else {
         return `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
