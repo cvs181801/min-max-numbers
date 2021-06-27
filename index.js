ed62c609ffee47f099c955cb5ed7cd2f5a76e583
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 10:37:37 -0700

    change the array to test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //create a way to find the maximum and minimum numbers in any number set.
 
-const numArray = [2, 5, 7, 0, 9, 3];
+const numArray = [2, 5, 17, 0, 9, -3];
 
 const findMax = Math.max(...numArray);
 
