d979d9b255ba0c7814c80b4a066607bdc39f5b9b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 10:45:30 -0700

    add the values into the function body

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 
 //create a way to find the maximum and minimum numbers in any number set.
 
-const numArray = [2, 5, 17, 0, 9, -3, 'P'];
+const numArray = [2, 5, 17, 0, 9, -3];
 
-const findMax = Math.max(...numArray);
 
-console.log(findMax);
 
-const findMin = Math.min(...numArray);
 
-console.log(findMin);
+
+
+
+function findMinMax(numArray) {
+    const findMax = Math.max(...numArray);
+    const findMin = Math.min(...numArray);
+    `The Largest Number: ${findMax}`;
+    `The Smallest Number: ${findMin}`;
+}
+
+console.log(findMinMax(numArray));
