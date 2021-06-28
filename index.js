91cb2828896fa7257791abb80a38262120f0a7bb
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-28 15:29:57 -0700

    try with new array name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 //grab the elements 
 
-//let randomNumberSet = document.querySelectorAll("randomNumbers");
+let randomNumberSet = document.querySelectorAll("randomNumbers");
 //console.log(randomNumberSet);
 
 const goButton = document.getElementById("go-btn");
@@ -10,17 +10,15 @@ const childDiv = document.createElement("p");
 
 //create a way to find the maximum and minimum numbers in any number set.
 
- let randomNumberSet = [2, 4, 7, 9, 5];
+ let newArray = [];
 
- function findMinMax(array) {
-    //randomNumberSet = document.querySelectorAll("randomNumbers");
-    //const newArray = Array.from(randomNumberSet);
-    //numArray = [...newArray];
-      const findMax = Math.max(...array);
-      const findMin = Math.min(...array);
-        console.log(findMax);
-        console.log(findMin);
-    //console.log(numArray);
+ function findMinMax(numbers) {
+     newArray = Array.from(numbers);
+    //newArray = [...numbers];
+      const findMax = Math.max(...newArray);
+      const findMin = Math.min(...newArray);
+        //console.log(findMax);
+        //console.log(findMin);
 
     if (Number.isNaN(findMax)) {
         childDiv.innerHTML = "";
@@ -38,6 +36,6 @@ const childDiv = document.createElement("p");
  goButton.addEventListener('click', function(e) {
      e.preventDefault();
      //console.log("it worked!!!");
-     return findMinMax(randomNumberSet);
+     return findMinMax(randomNumberSet.value);
  } )
 
