e5acf0a5d56d507f9d72fb4a1aac517b2a09b3d1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-28 14:50:02 -0700

    test button

--- index.js
+++ index.js
@@ -2,38 +2,38 @@
 //grab the elements 
 
 let randomNumberSet = document.querySelectorAll("randomNumbers");
-
 //console.log(randomNumberSet);
  const goButton = document.getElementById("go-btn");
-// const parentDiv = document.getElementById("resultDiv");
-// const childDiv = document.createElement("p");
+const parentDiv = document.getElementById("resultDiv");
+const childDiv = document.createElement("p");
 
-// create a way to find the maximum and minimum numbers in any number set.
-// //randomNumberSet = [4, 8, 9, 2, -10];
+//create a way to find the maximum and minimum numbers in any number set.
+
  let numArray = [];
 
  function findMinMax(randomNumberSet) {
     randomNumberSet = document.querySelectorAll("randomNumbers");
-     const newArray = Array.from(randomNumberSet);
-     numArray = [...newArray];
-//     const findMax = Math.max(...numArray);
-//     const findMin = Math.min(...numArray);
-     console.log(numArray);
-//     if (Number.isNaN(findMax)) {
-//         childDiv.innerHTML = "";
-//         childDiv.innerHTML = `Oops, try it using only numbers instead!`;
-//         parentDiv.append(childDiv);
-//     } else {
-//         childDiv.innerHTML = "";
-//         childDiv.innerHTML = `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
-//         parentDiv.append(childDiv);
-//     }
+    const newArray = Array.from(randomNumberSet);
+    numArray = [...newArray];
+      const findMax = Math.max(...numArray);
+      const findMin = Math.min(...numArray);
+     //console.log(numArray);
+    if (Number.isNaN(findMax)) {
+        childDiv.innerHTML = "";
+        childDiv.innerHTML = `Oops, try it using only numbers instead!`;
+        parentDiv.append(childDiv);
+    } else {
+        childDiv.innerHTML = "";
+        childDiv.innerHTML = `The Largest Number: ${findMax}, The Smallest Number: ${findMin}`;
+        parentDiv.append(childDiv);
+    }
  }
 
-// //console.log(findMinMax(randomNumberSet.value));
+//console.log(findMinMax(randomNumberSet.value));
 
  goButton.addEventListener('click', function(e) {
      e.preventDefault();
-     return findMinMax(randomNumberSet.value);
+     console.log("it worked!!!");
+     //return findMinMax(randomNumberSet.value);
  } )
 
