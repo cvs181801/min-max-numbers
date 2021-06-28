ff752283e9d0fb731a664b7cba51817b64467b93
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-27 17:18:03 -0700

    try it with getelementsbyclassname() instead

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 
 //grab the elements 
 
-const randomNumberSet = document.querySelectorAll("randomNumbers");
+const randomNumberSet = document.getElementsByClassName("randomNumbers");
 
 console.log(randomNumberSet);
  const goButton = document.getElementById("go-btn");
 // const parentDiv = document.getElementById("resultDiv");
 // const childDiv = document.createElement("p");
 
-
 // create a way to find the maximum and minimum numbers in any number set.
 // //randomNumberSet = [4, 8, 9, 2, -10];
  let numArray = [];
 
  function findMinMax(randomNumberSet) {
-     numArray.push(randomNumberSet);
+     numArray = [...randomNumberSet];
 //     const findMax = Math.max(...numArray);
 //     const findMin = Math.min(...numArray);
-     console.log(randomNumberSet);
+     console.log(numArray);
 //     if (Number.isNaN(findMax)) {
 //         childDiv.innerHTML = "";
 //         childDiv.innerHTML = `Oops, try it using only numbers instead!`;
