fa940e5603a57e669ff0e243d2a3414974555356
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-05 20:51:40 -0700

    try to get the values individual id's

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 //grab the elements 
 
-let randomNumberSet = document.querySelectorAll("#randomNumbers");
+let randomNumberSet1 = document.getElementById("randomNumber1");
+let randomNumberSet2 = document.getElementById("randomNumber2");
+let randomNumberSet3 = document.getElementById("randomNumber3");
+let randomNumberSet4 = document.getElementById("randomNumber4");
+let randomNumberSet5 = document.getElementById("randomNumber5");
 
-console.log(randomNumberSet);
+console.log(randomNumberSet1, randomNumberSet2, randomNumberSet3, randomNumberSet4, randomNumberSet5);
 
+console.log(randomNumberSet1.value, randomNumberSet2.value, randomNumberSet3.value, randomNumberSet4.value, randomNumberSet5.value);
 
 // const goButton = document.getElementById("go-btn");
 // const parentDiv = document.getElementById("resultDiv");
@@ -42,5 +47,7 @@ console.log(randomNumberSet);
 //      return findMinMax(randomNumberSet.value);
 //  } )
 
-console.log(randomNumberSet.value);
+
 console.log(parseInt(randomNumberSet.value));
+console.log(parseInt(randomNumberSet1.value), parseInt(randomNumberSet2.value), parseInt(randomNumberSet3.value), parseInt(randomNumberSet4.value), parseInt(randomNumberSet5.value));
+
